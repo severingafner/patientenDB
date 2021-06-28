998dc94cef935bb1abbb52f4608071b35f7689ce
Severin Gafner <severin.gafner@gmail.com>
2021-06-28 20:28:58 +0200

    Change namespace

--- patientDB/patientDB/DBConnection/DBConnection.cs
+++ patientDB/patientDB/DBConnection/DBConnection.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace patientDB.DBConnection
+namespace patientDB
 {
     public static class DBConnection
     {
--- patientDB/patientDB/Model/DBObject.cs
+++ patientDB/patientDB/Model/DBObject.cs
@@ -1,18 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using patientDB.DBConnection;
 
-namespace patientDB.Model
+namespace patientDB
 {
     class DBObject
     {
         public KeyValuePair<Attribute, Node>[] data { get; set; }
 
-        public void getDBObjectData()
+        public void insert ()
         {
+            SqlConnection conn = DBConnection.LocalInstance();
+            //Commandobjekt bilden
+            SqlCommand insertCommand = new SqlCommand(my_sql, conn);
 
+            try
+            {
+                //Connection öffnen
+                conn.Open();
+
+                //Befehl abschicken
+                insertCommand.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                //Fehlermeldung
+            }
+            finally
+            {
+                //DB-Connection schliessen
+                conn.Close();
+            }
         }
     }
 }
