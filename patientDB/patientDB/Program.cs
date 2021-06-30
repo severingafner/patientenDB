44afacfdec3ed313174610af5f8a38c2911b1c9e
Severin Gafner <severin.gafner@gmail.com>
2021-06-30 22:15:20 +0200

    log

--- patientDB/patientDB/Program.cs
+++ patientDB/patientDB/Program.cs
@@ -11,26 +11,34 @@ namespace patientDB
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Enter Path to file:");
             string pathToFile = Console.ReadLine();
 
+            Console.WriteLine("\n\nLoad DBObjects from XML");
             XDocument doc = XDocument.Load(pathToFile);
-            try
+            foreach (DBObject dbObj in ReadFile.Read(doc))
             {
-                foreach (DBObject dbObj in ReadFile.Read(doc))
-                {
-                    WriteOnDB.Write(dbObj, Connection.LocalInstance());
-                }
-                Console.ReadLine();
-
+                Console.WriteLine("Loaded DBObject from XML:\n");
+                logDBObject(dbObj);
+                Console.WriteLine("Write DBObject to DB");
+                int newObjectId = WriteOnDB.Write(dbObj, Connection.LocalInstance());
+                Console.WriteLine("Read DBObject from DB");
+                DBObject readObject = ReadFromDB.Read(newObjectId, Connection.LocalInstance());
+                Console.WriteLine("DBObject from DB:\n");
+                logDBObject(readObject);
             }
-            catch (Exception e)
+            Console.ReadLine();
+        }
+
+        static void logDBObject (DBObject dbObject)
+        {
+            Console.WriteLine(dbObject.value);
+            foreach (KeyValuePair<Attribute, Node> row in dbObject.data)
             {
-                Console.WriteLine("Error writing on DB");
-                Console.WriteLine(e);
-                Console.ReadLine();
-
+                Console.Write(row.Key.value + ' ' + row.Value.value + '\t');
             }
+            Console.WriteLine('\n');
         }
     }
 }
